f23bb35707ce67f3c6bb4f67d8147517c876f7e8
hicks27 <hicksp2597@student.faytechcc.edu>
2017-06-14 18:11:49 -0700

    Add files via upload

---
+++ M2HW1_DistanceTraveled_Hicks.py
@@ -0,0 +1,9 @@
+#Distance traveled.
+#6/14/17
+#CTI-110 M2HW1-Distance traveled
+#Patricia Hicks
+#
+print('6 hours * 70 miles per hour = 420 miles traveled')
+print('10 hour * 70 miles per hour = 700 miles traveled')
+print('15 hours * 70 mile per hour = 1,050 miles traveled')
+
